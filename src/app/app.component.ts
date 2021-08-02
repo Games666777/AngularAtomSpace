f282ef6fd6d643a209ea82696d7c6ef4aef804e3
Games666777 <47324881+Games666777@users.noreply.github.com>
2021-08-02 21:08:12 +0300

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+     
+class Item{
+    purchase: string;
+    done: boolean;
+    price: number;
+     
+    constructor(purchase: string, price: number) {
+  
+        this.purchase = purchase;
+        this.price = price;
+        this.done = false;
+    }
+}
+ 
+@Component({
+    selector: 'purchase-app',
+    template: `<div class="page-header">
+        <h1> Список дел </h1>
+    </div>
+    <div class="panel">
+        <div class="form-inline">
+            <div class="form-group">
+                <div class="col-md-8">
+                    <input class="form-control" [(ngModel)]="text" placeholder = "Название" />
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-6">
+                    <input type="number" class="form-control" [(ngModel)]="price" placeholder="Цена" />
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-offset-2 col-md-8">
+                    <button class="btn btn-default" (click)="addItem(text, price)">Добавить</button>
+                </div>
+            </div>
+        </div>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Дело</th>
+                    <th>Время</th>
+                    <th>Выполнено</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr *ngFor="let item of items">
+                    <td>{{item.purchase}}</td>
+                    <td>{{item.price}}</td>
+                    <td><input type="checkbox" [(ngModel)]="item.done" /></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>`
+})
+export class AppComponent { 
+    text: string = "";
+    price: number = 0;
+     
+    items: Item[] = 
+    [
+        { purchase: "Написать код", done: false, price: 2 },
+        { purchase: "Пойти погулять", done: false, price: 4 },
+        { purchase: "Пойти на курс ", done: true, price: 2 },
+        { purchase: "Побегать", done: false, price: 1 }
+    ];
+    addItem(text: string, price: number): void {
+         
+        if(text==null || text.trim()=="" || price==null)
+            return;
+        this.items.push(new Item(text, price));
+    }
+}
